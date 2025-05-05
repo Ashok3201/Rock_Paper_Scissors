3b3921a52cc06bf926de87e0eb94f6a58bd3f69e
Ashok Kumar <140838790+Ashok3201@users.noreply.github.com>
2025-05-05 19:03:10 +0000

    Write:logic to get the computer choice through Pseudocode add Pseudocode with actual logic code commitNumber:0006;

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Rock Paper Scissor")
+console.log("Rock-Paper-Scissor Game")
+
+//Write the logic to get the computer choice
+
+//start function declare name getComputerChoice
+function getComputerChoice() {
+    //declare randomNumber var assign random value between  1 to 3 using  math.random().
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    //if randomNumber==1 return "Rock"
+    if (randomNumber == 1) {
+        return "Rock";
+    }
+    //else if randomNumber==2 return "Paper"
+    else if (randomNumber == 2) {
+        return "Paper";
+    }
+    //else if randomNumber==3 return "Scissor"
+    else {
+        return "Scissor";
+    }
+}
+//call getComputerChoice(), return string store in a computerSelection var
+const computerSelection = getComputerChoice();
+//console.log() check output is correct via developer console
+console.log(computerSelection);//check
