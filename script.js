e4b86e3a4026b9500a1ccd2633214a30fce992ab
Ashok Kumar <140838790+Ashok3201@users.noreply.github.com>
2025-05-05 20:33:30 +0000

    Write:the logic to play a single round through Pseudocode add Pseudocode with actual logic code commitNumber:0012;

--- script.js
+++ script.js
@@ -29,20 +29,20 @@ console.log(computerSelection);//check
 
 //start function, assign name getHumanChoice(with parameter name string)
 function getHumanChoice() {
-//declure var human= pop a window for Enter choice in these Rock, Paper, Scissor. With the help of propt().
-let human = prompt("Type any one you choose in these below word \n Rock, Paper, Scissor\n R, P, S\n r, p, s");
-//if human=="Rock" return "Rock", ensure with other variations such as "rock","ROCK","r"and "R" with the help of || (or) operator
-if (human == "Rock" || human == "rock" || human == "ROCK" || human == "R" || human == "r") {
-    return "Rock";
-}
-//else if human=="Paper" return "Paper",ensure with other variations such as "paper","PAPER","p"and "P" with the help of || (or) operator
-else if (human == "Paper" || human == "paper" || human == "PAPER" || human == "P" || human == "p") {
-    return "Paper";
-}
-//else if human=="Scissor" return "Scissor",ensure with other variations such as "scissor","SCISSOR","s"and "S" with the help of || (or) operator
-else if (human == "Scissor" || human == "scissor" || human == "SCISSOR" || human == "S" || human == "s") {
-    return "Scissor";
-}
+    //declure var human= pop a window for Enter choice in these Rock, Paper, Scissor. With the help of propt().
+    let human = prompt("Type any one you choose in these below word \n Rock, Paper, Scissor\n R, P, S\n r, p, s");
+    //if human=="Rock" return "Rock", ensure with other variations such as "rock","ROCK","r"and "R" with the help of || (or) operator
+    if (human == "Rock" || human == "rock" || human == "ROCK" || human == "R" || human == "r") {
+        return "Rock";
+    }
+    //else if human=="Paper" return "Paper",ensure with other variations such as "paper","PAPER","p"and "P" with the help of || (or) operator
+    else if (human == "Paper" || human == "paper" || human == "PAPER" || human == "P" || human == "p") {
+        return "Paper";
+    }
+    //else if human=="Scissor" return "Scissor",ensure with other variations such as "scissor","SCISSOR","s"and "S" with the help of || (or) operator
+    else if (human == "Scissor" || human == "scissor" || human == "SCISSOR" || human == "S" || human == "s") {
+        return "Scissor";
+    }
 
 }
 //call getHumanChoice(), store in a humanSelection var
@@ -57,3 +57,68 @@ console.log(humanSelection);//check
 let humanScore = 0;
 //declare computerScore var assign initial value 0
 let computerScore = 0;
+
+
+//Write the logic to play a single round
+
+
+
+//start function, assign name playRound(with parameter name humanChoice, computerChoice)
+function playRound(humanChoice, computerChoice) {
+
+    //if humanChoice =="Rock" &&(and) computerChoice == "Paper" 
+    if (humanChoice == "Rock" && computerChoice == "Paper") {
+        //print console.log("You lose! Paper beats Rock") 
+        console.log("You lose! Paper beats Rock");
+        // and also return "computerScore"
+        return "computerScore";
+    }
+    //else if humanChoice =="Paper" &&(and) computerChoice == "Rock" 
+    else if (humanChoice == "Paper" && computerChoice == "Rock") {
+        // print console.log("You Win! Paper beats Rock")
+        console.log("You Win! Paper beats Rock");
+        // return "humanScore"
+        return "humanScore";
+    }
+    //else if (humanChoice == "Paper" &&(and) computerChoice == "Scissor")
+    else if (humanChoice == "Paper" && computerChoice == "Scissor") {
+        //print console.log("You lose! Scissor beats Paper")
+        console.log("You lose! Scissor beats Paper");
+        //return "computerScore"
+        return "computerScore";
+    }
+    //else if (humanChoice == "Scissor" &&(and) computerChoice == "Paper")
+    else if (humanChoice == "Scissor" && computerChoice == "Paper") {
+        // print console.log("You Win! Scissor beats Paper"); 
+        console.log("You Win! Scissor beats Paper");
+        // return "humanScore"
+    return "humanScore";
+    }
+    //else if (humanChoice == "Scissor" &&(and) computerChoice == "Rock")
+    else if (humanChoice == "Scissor" && computerChoice == "Rock") {
+        // print console.log("You lose! Rock beats Scissor") 
+        console.log("You lose! Rock beats Scissor");
+        // return "computerScore"
+        return "computerScore";
+    }
+    //else if (humanChoice == "Rock" &&(and) computerChoice == "Scissor")
+    else if (humanChoice == "Rock" && computerChoice == "Scissor") {
+        // print console.log("You Win! Rock beats Scissor")
+        console.log("You Win! Rock beats Scissor");
+        // return "humanScore"
+        return "humanScore";
+    }
+}
+//call playRound(with argu humanSelection, computerSelection), store in a roundWinner var
+let roundWinner = playRound(humanSelection, computerSelection);
+
+//Increment the humanScore or computerScore variable based on the round winner.
+if (roundWinner == "computerScore") {
+    computerScore += 1;
+}
+else {
+    humanScore += 1;
+}
+//console.log() check output is correct via developer console
+console.log("You score:" + humanScore +" "+ "Computer score:" + computerScore);//check
+
